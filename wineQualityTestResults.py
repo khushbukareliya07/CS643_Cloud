87ddd669949ba5ce4dd8f26bee9cf77676bc0ba1
Khushbu Kareliya <59030360+khushbukareliya07@users.noreply.github.com>
2020-12-09 17:33:01 -0500

    Update wineQualityTestResults.py

--- wineQualityTestResults.py
+++ wineQualityTestResults.py
@@ -9,6 +9,12 @@ from pyspark.mllib.evaluation import MulticlassMetrics
 import sys
 import pyspark.sql.functions as func
 import pyspark
+if(len(sys.argv) < 3):
+    print("Please provide testfilepth")
+    sys.exit(-1)
+
+modelpath=sys.argv[1]
+testfilepath = sys.argv[2] 
 
 
 conf = SparkConf().setAppName("Wine Quality Testing").setMaster("local[1]")
@@ -16,10 +22,13 @@ sc = SparkContext(conf=conf)
 
 spark = SparkSession.builder.getOrCreate()
 
-rf = RandomForestClassifier.load("s3://myprogrambucket/rfwine_model.model")
+rf = RandomForestClassifier.load(modelpath)
+#rf = RandomForestClassifier.load("s3://myprogrambucket/rfwine_model.model")
 
-defTest = spark.read.format('csv').options(header='true', inferSchema='true', delimiter=';').csv(
-    "s3://myprogrambucket/ValidationDataset.csv")
+defTest = spark.read.format('csv').options(header='true', inferSchema='true', delimiter=';').csv(testfilepath)
+
+#defTest = spark.read.format('csv').options(header='true', inferSchema='true', delimiter=';').csv(
+ #   "s3://myprogrambucket/ValidationDataset.csv")
 defTest.printSchema()
 
 featureColumns = [col for col in defTest.columns if (
@@ -44,6 +53,7 @@ predictionAndLabels.take(5)
 
 # Calculate the Accuracy, Precision and Recall
 
+"""
 cm = metrics.confusionMatrix().toArray()
 print(cm)
 
@@ -52,3 +62,6 @@ for i in range(3, 9):
     if metrics.precision(i) != 0:
         print(i, ", ", round(metrics.precision(i), 2), ", ", round(metrics.recall(i), 2),
               ", ", round(metrics.accuracy, 2), ", ", round(metrics.fMeasure(float(i), 1.0), 2))
+"""
+
+print("F1 score is %s"%(metrics.accuracy))
